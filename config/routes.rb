a247f4f38c4e6fdd42a787f021df44d87773aa2f
momoticone <bendrissmohamed01@gmail.com>
2017-10-31 17:15:59 +0100

    projet s05

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
    root 'users#index'
     
      get  '/new',  to: 'users#new'
+    
+    get    '/login',   to: 'sessions#login'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
     
   resources :users
 end
